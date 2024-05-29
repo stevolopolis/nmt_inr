05a37c247d37f72094253521a48841f868c9ebac
stevolopolis <stevenluots@gmail.com>
2024-05-29 09:42:50 +0800

    added no_io option to training scripts.

--- scripts/sweep_image.sh
+++ scripts/sweep_image.sh
@@ -7,44 +7,59 @@ cuda=0
 # ("05" "18" "20" "21")
 img='05'
 model=("siren" "ffn")     # t
-lr=('0.01' '0.001' '0.0001' '0.00001')     # k
+lr=('0.001' '0.0001' '0.00001')     # k
 coord_mode="2"     
 data_range="0"     
 rff_std=("10" "11" "12" "13" "14")  # i
 lr_scheduler="constant"  # "cosine"
 scheduler="constant"
-strategy="void"
-ratio=("0.2" "0.4" "0.6" "0.8") # j
-iterations=2500
+strategy=("void" "dense")  #j
+ratio=("0.2" "0.4" "0.6" "0.8") #l
+top_k=("1" "0")     # i
+iterations=5000
 
 wandb_project=sgd
+optimizer=adam
 
 #TRAIN_CONFIGS.out_dir="${model[t]}_${rff_std[i]}_lr${lr[k]}_${coord_mode[j]}_kodak${img}" \
 #NETWORK_CONFIGS.rff_std="${rff_std[i]}" \
 # Train on Kodak dataset
 for t in {0..0};
 do 
-    for k in {0..3};
+    for k in {0..0};
     do
-        for j in {0..3};
+        for j in {1..1};
         do
-            python train_image.py DATASET_CONFIGS.file_path="../datasets/kodak/kodim${img}.png" \
-            TRAIN_CONFIGS.out_dir="${model[t]}_mt${ratio[j]}_lr${lr[k]}_camera" \
-            TRAIN_CONFIGS.device="cuda:${cuda}" \
-            TRAIN_CONFIGS.sampling_path="logs/sampling/sweep_${model[t]}_mt${ratio[j]}_lr${lr[k]}_camera.pkl" \
-            TRAIN_CONFIGS.loss_path="logs/loss/sweep_${model[t]}_mt${ratio[j]}_lr${lr[k]}_camera.pkl" \
-            TRAIN_CONFIGS.iterations="${iterations}" \
-            EXP_CONFIGS.mt_ratio="${ratio}" \
-            EXP_CONFIGS.scheduler_type="${scheduler}" \
-            EXP_CONFIGS.optimizer_type="sgd" \
-            EXP_CONFIGS.lr_scheduler_type="${lr_scheduler}" \
-            EXP_CONFIGS.strategy_type="${strategy}" \
-            NETWORK_CONFIGS.lr="${lr[k]}" \
-            NETWORK_CONFIGS.data_range="${data_range}" \
-            NETWORK_CONFIGS.coord_mode="${coord_mode}" \
-            WANDB_CONFIGS.group="${model[t]}" \
-            WANDB_CONFIGS.wandb_project="${wandb_project}" \
-            model_config="${model[t]}" &> "logs/logs/sweep_${model[t]}_mt${ratio[j]}_lr${lr[k]}_camera.pkl"
+            for i in {0..0};
+            do
+                for l in {0..1};
+                do
+                    # python train_image.py DATASET_CONFIGS.file_path="../datasets/kodak/kodim05.png" \
+                    # TRAIN_CONFIGS.out_dir="${optimizer}_${model[t]}_mt${ratio[l]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_kodak05" \
+                    # TRAIN_CONFIGS.sampling_path="logs/sampling/${optimizer}_${model[t]}_mt${ratio[l]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_kodak05.pkl" \
+                    # TRAIN_CONFIGS.loss_path="logs/loss/${optimizer}_${model[t]}_mt${ratio[l]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_kodak05.pkl" \
+                    #model_config="${model[t]}" &> "logs/logs/${optimizer}_${model[t]}_mt${ratio[l]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_kodak05.txt"
+                    python train_image.py DATASET_CONFIGS.file_path="camera" \
+                    TRAIN_CONFIGS.out_dir="${optimizer}_${model[t]}_mt${ratio[l]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_camera" \
+                    TRAIN_CONFIGS.sampling_path="logs/sampling/${optimizer}_${model[t]}_mt${ratio[l]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_camera.pkl" \
+                    TRAIN_CONFIGS.loss_path="logs/loss/${optimizer}_${model[t]}_mt${ratio[l]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_camera.pkl" \
+                    DATASET_CONFIGS.color_mode="L" \
+                    TRAIN_CONFIGS.device="cuda:${cuda}" \
+                    TRAIN_CONFIGS.iterations="${iterations}" \
+                    EXP_CONFIGS.mt_ratio="${ratio[l]}" \
+                    EXP_CONFIGS.scheduler_type="${scheduler}" \
+                    EXP_CONFIGS.optimizer_type="${optimizer}" \
+                    EXP_CONFIGS.lr_scheduler_type="${lr_scheduler}" \
+                    EXP_CONFIGS.strategy_type="${strategy[j]}" \
+                    EXP_CONFIGS.top_k="${top_k[i]}" \
+                    NETWORK_CONFIGS.lr="${lr[k]}" \
+                    NETWORK_CONFIGS.data_range="${data_range}" \
+                    NETWORK_CONFIGS.coord_mode="${coord_mode}" \
+                    WANDB_CONFIGS.group="${model[t]}" \
+                    WANDB_CONFIGS.wandb_project="${wandb_project}" \
+                    model_config="${model[t]}" &> "logs/logs/${optimizer}_${model[t]}_mt${ratio[l]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_camera.txt"
+                done
+            done
         done
     done
 done
--- scripts/train_image_with_strat.sh
+++ scripts/train_image_with_strat.sh
@@ -1,39 +1,44 @@
 #!/usr/bin/bash
 
-cuda=1
-wandb_group=kodak
-wandb_project=strategic_mt
+cuda=0
+wandb_group=video_demo_full
+wandb_project=mt_visualization
 
 # full training set
 # ("07" "14" "15" "17")
 # ("05" "18" "20" "21")
-model=("siren" "ffn")     # t
+model=("siren")     # t
 #img=("05" "18" "20" "21" "07" "14" "15" "17")      # k
 img=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24")
-scheduler="step"   
+scheduler="step"  
 lr_scheduler="cosine"
-strategy=("void" "incremental" "dense")    # j
+strategy=("incremental")    # j
+top_k=("1")     # i
 ratio=0.2   # 1.0 for reverse mode; 0.2 for normal mode
 
 # Train on Kodak dataset
 for t in {0..0};
 do
-    for k in {0..0};
+    for k in {13..23};
     do
         for j in {1..1};
         do
-            python train_image.py DATASET_CONFIGS.file_path="../datasets/kodak/kodim${img[k]}.png" \
-            TRAIN_CONFIGS.out_dir="${model[t]}_${strategy[j]}_${scheduler}_${lr_scheduler}_kodak${img[k]}" \
-            TRAIN_CONFIGS.device="cuda:${cuda}" \
-            TRAIN_CONFIGS.sampling_path="logs/sampling/${wandb_group}_${model[t]}_${strategy[j]}_${scheduler}_${lr_scheduler}_kodak${img[k]}.pkl" \
-            EXP_CONFIGS.mt_ratio="${ratio}" \
-            EXP_CONFIGS.scheduler_type="${scheduler}" \
-            EXP_CONFIGS.optimizer_type="adam" \
-            EXP_CONFIGS.lr_scheduler_type="${lr_scheduler}" \
-            EXP_CONFIGS.strategy_type="${strategy[j]}" \
-            WANDB_CONFIGS.group="${wandb_group}" \
-            WANDB_CONFIGS.wandb_project="${wandb_project}" \
-            model_config="${model[t]}" &> "logs/logs/${wandb_group}_${model[t]}_${strategy[j]}_${scheduler}_${lr_scheduler}_kodak${img[k]}.txt"
+            for i in {1..1};
+            do 
+                python train_image.py DATASET_CONFIGS.file_path="../datasets/kodak/kodim${img[k]}.png" \
+                TRAIN_CONFIGS.out_dir="${model[t]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_kodak${img[k]}" \
+                TRAIN_CONFIGS.device="cuda:${cuda}" \
+                TRAIN_CONFIGS.sampling_path="logs/sampling/${wandb_group}_${model[t]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_kodak${img[k]}.pkl" \
+                EXP_CONFIGS.mt_ratio="${ratio}" \
+                EXP_CONFIGS.scheduler_type="${scheduler}" \
+                EXP_CONFIGS.optimizer_type="adam" \
+                EXP_CONFIGS.lr_scheduler_type="${lr_scheduler}" \
+                EXP_CONFIGS.strategy_type="${strategy[j]}" \
+                EXP_CONFIGS.top_k="${top_k[i]}" \
+                WANDB_CONFIGS.group="${wandb_group}" \
+                WANDB_CONFIGS.wandb_project="${wandb_project}" \
+                model_config="${model[t]}" &> "logs/logs/${wandb_group}_${model[t]}_${strategy[j]}_${scheduler}_${lr_scheduler}_topk${top_k[i]}_kodak${img[k]}.txt"
+            done
         done
     done    
 done
